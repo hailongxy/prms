6b5fee51279c5faf91443548fa016eb05414054b
hailongxy <79578501+hailongxy@users.noreply.github.com>
2025-06-23 14:55:04 +0800

    增加go,react
    增加go,react

--- php/controller/c_knowledge.php
+++ php/controller/c_knowledge.php
@@ -2,7 +2,7 @@
 
 if($_GET['act'] == 'add_save'){
 		
-	$sql="INSERT INTO knowledge (title,parent_id,createtime,userid) VALUES ('新知识','".$_POST['add_parent_id']."',NOW(),'".$_SESSION['userid']."')";
+	$sql="INSERT INTO knowledge_item (title,parent_id,createtime,userid) VALUES ('新知识','".$_POST['add_parent_id']."',NOW(),'".$_SESSION['userid']."')";
 	
 	if(!mysqli_query($con,$sql)){
 		die('Error: ' . mysqli_error($con));
@@ -10,7 +10,7 @@ if($_GET['act'] == 'add_save'){
 	echo 1;
 }else if($_GET['act'] == 'add_top_save'){
 		
-	$sql="INSERT INTO knowledge (title,parent_id,createtime,userid) VALUES ('新知识','0',NOW(),'".$_SESSION['userid']."')";
+	$sql="INSERT INTO knowledge_item (title,parent_id,createtime,userid) VALUES ('新知识','0',NOW(),'".$_SESSION['userid']."')";
 	
 	if(!mysqli_query($con,$sql)){
 		die('Error: ' . mysqli_error($con));
@@ -19,7 +19,7 @@ if($_GET['act'] == 'add_save'){
 
 }else if($_GET['act'] == 'pre_update'){
 	
-	$sql = mysqli_query($con,"SELECT * FROM knowledge WHERE id ='".$_POST['id']."' and userid = '".$_SESSION['userid']."'");
+	$sql = mysqli_query($con,"SELECT * FROM knowledge_item WHERE id ='".$_POST['id']."' and userid = '".$_SESSION['userid']."'");
 	
 	$row = mysqli_fetch_array($sql);
 	
@@ -27,7 +27,7 @@ if($_GET['act'] == 'add_save'){
 }else if($_GET['act'] == 'update_save'){
 	
 	
-	$sql="UPDATE knowledge SET metric = '".$_POST['update_metric']."',content = '".$_POST['update_content']."' WHERE id = '".$_POST['id']."' and userid = '".$_SESSION['userid']."'";
+	$sql="UPDATE knowledge_item SET metric = '".$_POST['update_metric']."',content = '".$_POST['update_content']."' WHERE id = '".$_POST['id']."' and userid = '".$_SESSION['userid']."'";
 	
 	if(!mysqli_query($con,$sql)){
 		die('Error: ' . mysqli_error($con));
@@ -36,7 +36,7 @@ if($_GET['act'] == 'add_save'){
 }else if($_GET['act'] == 'update_name_save'){
 	
 	
-	$sql="UPDATE knowledge SET title = '".$_POST['name']."' WHERE id = '".$_POST['id']."' and userid = '".$_SESSION['userid']."'";
+	$sql="UPDATE knowledge_item SET title = '".$_POST['name']."' WHERE id = '".$_POST['id']."' and userid = '".$_SESSION['userid']."'";
 	
 	if(!mysqli_query($con,$sql)){
 		die('Error: ' . mysqli_error($con));
@@ -45,7 +45,7 @@ if($_GET['act'] == 'add_save'){
 }else if($_GET['act'] == 'update_parent_id_save'){
 	
 	
-	$sql="UPDATE knowledge SET parent_id = '".$_POST['parent_id']."' WHERE id = '".$_POST['id']."' and userid = '".$_SESSION['userid']."'";
+	$sql="UPDATE knowledge_item SET parent_id = '".$_POST['parent_id']."' WHERE id = '".$_POST['id']."' and userid = '".$_SESSION['userid']."'";
 	
 	if(!mysqli_query($con,$sql)){
 		die('Error: ' . mysqli_error($con));
@@ -53,7 +53,7 @@ if($_GET['act'] == 'add_save'){
 	echo 1;
 }else if($_GET['act'] == 'delete'){
 	
-	$sql="DELETE FROM knowledge WHERE id='".$_POST['id']."' and userid = '".$_SESSION['userid']."'";
+	$sql="DELETE FROM knowledge_item WHERE id='".$_POST['id']."' and userid = '".$_SESSION['userid']."'";
 	
 	if(!mysqli_query($con,$sql)){
 		die('Error: ' . mysqli_error($con));
